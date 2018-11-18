fa32f2fd37b650e9933645cb36f83300bc42f52c
andreas <andreas.peter.ch@gmail.com>
2018-11-18 18:45:07 +0000

    Export some functions

--- src/MatrixProductStates.jl
+++ src/MatrixProductStates.jl
@@ -6,8 +6,13 @@ using KrylovKit
 abstract type AbstractMPS{L,T,TE,TB} end
 abstract type AbstractMPO{L,T,TE,TB} end
 
+export MPS, CanonicalMPS, MPO
+export centerlink, zerosite, randu1mps, inner, canonicalize, canonicalize!, normalize!, normalize
+export onesiteexp, onesitevar, twositecorr, applyonesite!, entropy
 include("mps.jl")
+export dmrg
 include("dmrg.jl")
+export hubbardMPO
 include("hubbard.jl")
 
 
